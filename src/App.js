90ea08686fcccc49f2a9436d2b8a9cb67c8ebded
Jasmine English <thisjasmine0@gmail.com>
2018-09-27 11:52:10 -0400

    made player component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import PodcastPlayer from './client/components/PodcastPlayer'
 
 class App extends Component {
   render() {
@@ -11,11 +12,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <PodcastPlayer />
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

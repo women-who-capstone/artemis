2f448fbd7c42aff2ae300ef9f8294cebc40324fa
Jillian McLaren <jillian.mclaren@gmail.com>
2018-09-27 12:58:05 -0400

    channels api

---
+++ server/api/channel.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+
+const { Channel, Tag, Genre } = require("../db/models");
+
+router.get("/", async (req, res, next) => {
+  try {
+    if (req.query) {
+      const channels = await Channel.findAll({
+        where: req.query
+      });
+      res.status(200).send(channels);
+    } else {
+      const channels = await Channel.findAll({});
+      res.status(200).send(channels);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const channel = await Channel.findById(id, {
+      include: [
+        {
+          model: Tag
+        },
+        {
+          model: Genre
+        }
+      ]
+    });
+    res.status(200).send(channel);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,11 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-module.exports = router
+router.use("/channel", require("./channel"));
+
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+module.exports = router;

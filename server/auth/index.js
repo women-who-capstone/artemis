a56b4aa2682f48637c7178f003272ea0d71ee814
Jasmine English <thisjasmine0@gmail.com>
2018-09-28 11:36:09 -0400

    renaming branch

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,22 +2,22 @@ const router = require('express').Router()
 const { User } = require('../db/models/')
 module.exports = router
 
-router.post('/login', async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { email: req.body.email } })
-    if (!user) {
-      console.log('No such user found:', req.body.email)
-      res.status(401).send('Wrong username and/or password')
-    } else if (!user.correctPassword(req.body.password)) {
-      console.log('Incorrect password for user:', req.body.email)
-      res.status(401).send('Wrong username and/or password')
-    } else {
-      req.login(user, err => (err ? next(err) : res.json(user)))
-    }
-  } catch (err) {
-    next(err)
-  }
-})
+// router.post('/login', async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ where: { email: req.body.email } })
+//     if (!user) {
+//       console.log('No such user found:', req.body.email)
+//       res.status(401).send('Wrong username and/or password')
+//     } else if (!user.correctPassword(req.body.password)) {
+//       console.log('Incorrect password for user:', req.body.email)
+//       res.status(401).send('Wrong username and/or password')
+//     } else {
+//       req.login(user, err => (err ? next(err) : res.json(user)))
+//     }
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 router.post('/signup', async (req, res, next) => {
   try {
@@ -32,11 +32,11 @@ router.post('/signup', async (req, res, next) => {
   }
 })
 
-router.post('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
-})
+// router.post('/logout', (req, res) => {
+//   req.logout()
+//   req.session.destroy()
+//   res.redirect('/')
+// })
 
 router.get('/me', (req, res) => {
   res.json(req.user)

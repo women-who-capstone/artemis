8de96eca94c3e9c13c045fc885c479ee106c82b7
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-10-01 15:18:02 -0400

    Created ChannelListItem component

---
+++ client/components/ChannelListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from '@material-ui/core/ListItemText'
+
+const ChannelListItem = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ChannelListItem;

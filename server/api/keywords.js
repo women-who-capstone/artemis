3c04fed9564d8283566565b25c6644795b2cf008
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-10-02 20:28:57 -0400

    using natural.js for geting score of each word in description

--- server/api/keywords.js
+++ server/api/keywords.js
@@ -1,34 +1,65 @@
 const router = require('express').Router();
 const unirest = require('unirest');
 const algorithmia = require('algorithmia');
-// const csvDescription = require('./podcasts.csv');
-var csv = require('fast-csv');
+var timeout = require('connect-timeout');
 
-router.get('/', async (req, res, next) => {
-	let dataParsed = csv
-		.fromPath('podcastsData.csv', { headers: [ 'Description' ] })
-		.on('data', function(data) {
-			console.log(data);
-		})
-		.on('end', function() {
-			console.log('done');
-		});
+const fs = require('fs');
+const csv = require('fast-csv');
+const path = require('path');
+
+var natural = require('natural');
+var TfIdf = natural.TfIdf;
+var tfidf = new TfIdf();
+
+var stream = fs.createReadStream(path.join(__dirname, 'podcasts.csv'));
+const headers = [ , , , , , , , , 'Description' ];
+
+let descriptions = [];
+csv
+	.fromStream(stream, { headers: headers })
+	.on('data', function(data) {
+		if (descriptions.length < 10) {
+			descriptions.push(data['Description']);
+		} else {
+			return;
+		}
+	})
+	.on('end', function() {
+		console.log('done');
+	});
+
+router.get('/', timeout(1000000), async (req, res, next) => {
 	try {
-		let input = req.query.input;
-		input = input.map((e) => JSON.parse(e));
-		console.log('req', input);
-		algorithmia.client(process.env.apiKey).algo(dataParsed).pipe(input).then(function(output) {
-			if (output.error) {
-				console.log(output.error);
-				result = output.error;
-			} else {
-				console.log(output.get());
-				result = output.get();
-			}
+		// let input = req.query.input;
+		//input = input.map(e => JSON.parse(e));
+
+		let input = descriptions.join(' ');
+		console.log('this is input', input);
+		tfidf.addDocument(input);
+		console.log('node--------------');
+		// tfidf.tfidfs('health', function(i, measure) {
+		// 	console.log('document # ' + i + ' is ' + measure);
+		// });
+		tfidf.listTerms(0 /*document index*/).forEach(function(item) {
+			console.log(item.term + ': ' + item.tfidf);
 		});
+
+		// algorithmia
+		// 	.client(process.env.apiKey)
+		// 	.algo('nlp/KeywordsForDocumentSet/0.1.7')
+		// 	.pipe(input)
+		// 	.then(function(output) {
+		// 		if (output.error) {
+		// 			console.log(output.error);
+		// 			result = output.error;
+		// 		} else {
+		// 			console.log(output.get());
+		// 			result = output.get();
+		// 		}
+		// 	});
+		// res.send(result);
 	} catch (err) {
 		next(err);
 	}
 });
-
 module.exports = router;

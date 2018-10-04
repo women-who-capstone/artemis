e89f87284fd58a211d4d4918a12c04b912d33208
Jillian McLaren <jillian.mclaren@gmail.com>
2018-10-04 17:06:13 -0400

    tag trial

--- server/db/models/channelTag.js
+++ server/db/models/channelTag.js
@@ -2,6 +2,11 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const ChannelTag = db.define("channelTag", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   score: {
     type: Sequelize.INTEGER
   }

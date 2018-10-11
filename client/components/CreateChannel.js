5fb28881ba78939a86791903d9f472dd4d9cdf61
Jasmine English <thisjasmine0@gmail.com>
2018-10-11 12:11:12 -0400

    added instructions to createChannel

--- client/components/CreateChannel.js
+++ client/components/CreateChannel.js
@@ -1,24 +1,27 @@
-import React from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-import { Redirect } from "react-router";
-import deburr from "lodash/deburr";
-import Autosuggest from "react-autosuggest";
-import match from "autosuggest-highlight/match";
-import parse from "autosuggest-highlight/parse";
-import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
-import MenuItem from "@material-ui/core/MenuItem";
-import Button from "@material-ui/core/Button";
-import Channel from "./Channel";
-import { withStyles } from "@material-ui/core/styles";
-import { setSinglePodcast, setPodcastList, fetchCategoryPodcastsEpisodeData } from "../reducers/podcast";
-import { connect } from "react-redux";
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import {Redirect} from 'react-router';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import {TextField, Typography} from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+import {
+  setSinglePodcast,
+  setPodcastList,
+  fetchCategoryPodcastsEpisodeData
+} from '../reducers/podcast';
+import {connect} from 'react-redux';
 
 let suggestions = [];
 
 async function fetchAPIGenres() {
-  let res = await axios.get("/api/genre/apilist");
+  let res = await axios.get('/api/genre/apilist');
   let APIgenresList = res.data.genres;
   suggestions = APIgenresList.map(genre => ({
     label: genre.name,
@@ -26,27 +29,64 @@ async function fetchAPIGenres() {
   }));
 }
 
+const styles = theme => ({
+  root: {
+    height: 250,
+    flexGrow: 1
+  },
+  container: {
+    position: 'relative'
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0
+  },
+  suggestion: {
+    display: 'block'
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none'
+  },
+  divider: {
+    height: theme.spacing.unit * 2
+  },
+  input: {
+    marginTop: '1rem'
+  }
+});
+
 function renderInputComponent(inputProps) {
-  const { classes, inputRef = () => {}, ref, ...other } = inputProps;
+  const {classes, inputRef = () => {}, ref, ...other} = inputProps;
 
   return (
-    <TextField
-      fullWidth
-      InputProps={{
-        inputRef: node => {
-          ref(node);
-          inputRef(node);
-        },
-        classes: {
-          input: classes.input
-        }
-      }}
-      {...other}
-    />
+    <div>
+      <Typography variant="display1">Create a channel</Typography>
+      <Typography variant="subheading">
+        Start typing below to select from our list of genres.
+      </Typography>
+      <TextField
+        fullWidth
+        InputProps={{
+          inputRef: node => {
+            ref(node);
+            inputRef(node);
+          },
+          classes: {
+            input: classes.input
+          }
+        }}
+        {...other}
+      />
+    </div>
   );
 }
 
-function renderSuggestion(suggestion, { query, isHighlighted }) {
+function renderSuggestion(suggestion, {query, isHighlighted}) {
   const matches = match(suggestion.label, query);
   const parts = parse(suggestion.label, matches);
 
@@ -55,11 +95,11 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
       <div>
         {parts.map((part, index) => {
           return part.highlight ? (
-            <span key={String(index)} style={{ fontWeight: 500 }}>
+            <span key={String(index)} style={{fontWeight: 500}}>
               {part.text}
             </span>
           ) : (
-            <strong key={String(index)} style={{ fontWeight: 300 }}>
+            <strong key={String(index)} style={{fontWeight: 300}}>
               {part.text}
             </strong>
           );
@@ -93,43 +133,15 @@ function getSuggestionValue(suggestion) {
   return suggestion.label;
 }
 
-const styles = theme => ({
-  root: {
-    height: 250,
-    flexGrow: 1
-  },
-  container: {
-    position: "relative"
-  },
-  suggestionsContainerOpen: {
-    position: "absolute",
-    zIndex: 1,
-    marginTop: theme.spacing.unit,
-    left: 0,
-    right: 0
-  },
-  suggestion: {
-    display: "block"
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: "none"
-  },
-  divider: {
-    height: theme.spacing.unit * 2
-  }
-});
-
 class IntegrationAutosuggest extends React.Component {
   state = {
-    single: "",
+    single: '',
     suggestions: [],
     id: 0,
     reDirect: false
   };
 
-  handleSuggestionsFetchRequested = ({ value }) => {
+  handleSuggestionsFetchRequested = ({value}) => {
     this.setState({
       suggestions: getSuggestions(value)
     });
@@ -141,7 +153,7 @@ class IntegrationAutosuggest extends React.Component {
     });
   };
 
-  handleChange = name => (event, { newValue }) => {
+  handleChange = name => (event, {newValue}) => {
     this.setState({
       [name]: newValue
     });
@@ -160,9 +172,11 @@ class IntegrationAutosuggest extends React.Component {
       let res = await axios.get(`/api/podcast?id=${genreId}`);
       let channelList = res.data;
 
-      if (channelList.channels === undefined) throw new Error('channelList is undefined')
+      if (channelList.channels === undefined) {
+        throw new Error('channelList is undefined');
+      }
 
-      this.props.fetchCategoryPodcastsEpisodeData(channelList.channels)
+      this.props.fetchCategoryPodcastsEpisodeData(channelList.channels);
 
       // let randomPodcast =
       //   channelList.channels[Math.floor(Math.random() * channelList.channels.length + 1)];
@@ -170,7 +184,7 @@ class IntegrationAutosuggest extends React.Component {
       // this.props.setSinglePodcast(randomPodcast);
       // this.props.setPodcastList(channelList.channels);
 
-      const createdChannel = await axios.post("/api/channel", {
+      const createdChannel = await axios.post('/api/channel', {
         name: searchInput
       });
       this.setState({
@@ -178,12 +192,12 @@ class IntegrationAutosuggest extends React.Component {
         reDirect: true
       });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
 
     const autosuggestProps = {
       renderInputComponent,
@@ -205,9 +219,9 @@ class IntegrationAutosuggest extends React.Component {
             {...autosuggestProps}
             inputProps={{
               classes,
-              placeholder: "Search",
+              placeholder: 'Search',
               value: this.state.single,
-              onChange: this.handleChange("single")
+              onChange: this.handleChange('single')
             }}
             theme={{
               container: classes.container,
@@ -222,7 +236,9 @@ class IntegrationAutosuggest extends React.Component {
             )}
           />
           <div className={classes.divider} />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" color="secondary">
+            Create Channel
+          </Button>
         </form>
       </div>
     );
@@ -233,7 +249,8 @@ const mapDispatchToProps = dispatch => {
   return {
     // setSinglePodcast: podcast => dispatch(setSinglePodcast(podcast)),
     // setPodcastList: podcasts => dispatch(setPodcastList(podcasts)),
-    fetchCategoryPodcastsEpisodeData: podcasts => dispatch(fetchCategoryPodcastsEpisodeData(podcasts))
+    fetchCategoryPodcastsEpisodeData: podcasts =>
+      dispatch(fetchCategoryPodcastsEpisodeData(podcasts))
   };
 };
 
